78b39adc165529e3ebfa46a082572ad462b1513e
Marco <marco.vanin@epsi.fr>
2020-02-05 16:33:04 +0100

    Add all Servlet to do

---
+++ src/main/java/com/mycompany/mavenproject1/ExtractPageServlet.java
@@ -0,0 +1,19 @@
+package com.mycompany.mavenproject1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ExtractPageServlet")
+public class ExtractPageServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+}
