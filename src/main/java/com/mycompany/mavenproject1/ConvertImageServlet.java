26f5d213c55b9e692b9a7547a7a4542b61e79cc7
Marco <marco.vanin@epsi.fr>
2020-02-05 16:30:22 +0100

    103e commit inutile

--- src/main/java/com/mycompany/mavenproject1/ConvertImageServlet.java
+++ src/main/java/com/mycompany/mavenproject1/ConvertImageServlet.java
@@ -5,14 +5,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(name = "ConvertImageServlet")
 public class ConvertImageServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, java.io.IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
 
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, java.io.IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response){
 
     }
 }
