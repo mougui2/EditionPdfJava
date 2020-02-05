f9c3ac35d0405f2f9095c454da32f5d629827136
Marco <marco.vanin@epsi.fr>
2020-02-05 16:28:00 +0100

    add ConvertImageServlet as a real Servlet as a real servlet in the right folder

--- src/main/java/com/mycompany/mavenproject1/ConvertImageServlet.java
+++ src/main/java/com/mycompany/mavenproject1/ConvertImageServlet.java
@@ -1,4 +1,18 @@
 package com.mycompany.mavenproject1;
 
-public class ConvertImageServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "ConvertImageServlet")
+public class ConvertImageServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, java.io.IOException {
+
+    }
 }
