2a589bc6eff490b30ca8823ab551ed6934573e48
mougui2 <57803410+mougui2@users.noreply.github.com>
2020-02-11 21:36:25 +0100

    extractpage implementation

--- src/main/java/com/mycompany/mavenproject1/ExtractPageServlet.java
+++ src/main/java/com/mycompany/mavenproject1/ExtractPageServlet.java
@@ -7,18 +7,58 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import static java.lang.Integer.parseInt;
 
-@WebServlet(name = "ExtractPageServlet")
+@WebServlet(name = "ExtractPageServlet", urlPatterns="/ExtractPageServlet")
 public class ExtractPageServlet extends HttpServlet {
    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         
-        int pageNumber = int.class.cast( request.getParameter("page"));
+        int pageNumber = parseInt( request.getParameter("page"));
         String filePath = request.getParameter("path");
         File f = new File(filePath);
         String destinationPath = System.getProperty("user.home") + File.separator + "Downloads"+File.separator+f.getName()+".pdf";
         PdfUtil.ExtractPage(pageNumber,filePath,destinationPath);
     }
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
 }
